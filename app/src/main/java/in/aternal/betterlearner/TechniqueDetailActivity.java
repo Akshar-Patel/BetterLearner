fe61b57b48435812f12f7a494bbbc93bc40226f4
Akshar Patel <akshar26@gmail.com>
2018-02-13 22:21:02 +0530

    feature: add what fragment

--- app/src/main/java/in/aternal/betterlearner/TechniqueDetailActivity.java
+++ app/src/main/java/in/aternal/betterlearner/TechniqueDetailActivity.java
@@ -1,13 +1,18 @@
 package in.aternal.betterlearner;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import in.aternal.betterlearner.WhatFragment.OnFragmentInteractionListener;
 
-public class TechniqueDetailActivity extends AppCompatActivity {
+public class TechniqueDetailActivity extends AppCompatActivity implements
+    OnFragmentInteractionListener {
 
   private TextView mTextMessage;
 
@@ -17,14 +22,20 @@ public class TechniqueDetailActivity extends AppCompatActivity {
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
       switch (item.getItemId()) {
-        case R.id.navigation_home:
-          mTextMessage.setText(R.string.title_home);
+        case R.id.navigation_what:
+          mTextMessage.setText(R.string.title_what);
+          WhatFragment whatFragment = WhatFragment.newInstance("test1","test2");
+          FragmentManager fragmentManager = getSupportFragmentManager();
+          FragmentTransaction fragmentTransaction = fragmentManager
+              .beginTransaction();
+          fragmentTransaction.add(R.id.frame_layout_fragment_container,
+              whatFragment).addToBackStack(null).commit();
           return true;
-        case R.id.navigation_dashboard:
-          mTextMessage.setText(R.string.title_dashboard);
+        case R.id.navigation_why:
+          mTextMessage.setText(R.string.title_why);
           return true;
-        case R.id.navigation_notifications:
-          mTextMessage.setText(R.string.title_notifications);
+        case R.id.navigation_how:
+          mTextMessage.setText(R.string.title_how);
           return true;
       }
       return false;
@@ -39,6 +50,17 @@ public class TechniqueDetailActivity extends AppCompatActivity {
     mTextMessage = (TextView) findViewById(R.id.message);
     BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    WhatFragment whatFragment = WhatFragment.newInstance("test1","test2");
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager
+        .beginTransaction();
+    fragmentTransaction.add(R.id.frame_layout_fragment_container,
+        whatFragment).addToBackStack(null).commit();
   }
 
+  @Override
+  public void onFragmentInteraction(Uri uri) {
+
+  }
 }
